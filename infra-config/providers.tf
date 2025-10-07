bd9e41f8b27cb9a738f15d3df6d18f892fe55860
benmiles-hashi <ben.miles@hashicorp.com>
2025-10-07 17:03:56 -0400

    fix

--- infra-config/providers.tf
+++ infra-config/providers.tf
@@ -14,10 +14,21 @@ terraform {
       source  = "hashicorp/random"
       version = "3.7.2"
     }
+    aap = {
+      source = "ansible/aap"
+      version = "1.4.0-devpreview1"
+    }
   }
 }
 provider "vault" {
   address = var.vault_address
   token   = var.vault_root_token
   namespace = var.vault_namespace
+}
+provider "aap" {
+  # configure with your AAP/Tower host & credentials
+  host = var.aap_hostname
+  username = var.aap_username
+  password = var.aap_password
+  insecure_skip_verify = true
 }

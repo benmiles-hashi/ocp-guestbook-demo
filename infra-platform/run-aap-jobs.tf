edb017095c0e763c8af2ad3fc4dc99a82f8904bd
benmiles-hashi <ben.miles@hashicorp.com>
2025-10-09 11:01:15 -0400

    added approle job

--- infra-platform/run-aap-jobs.tf
+++ infra-platform/run-aap-jobs.tf
@@ -13,6 +13,24 @@ data "aap_job_template" "vso_operator_install" {
   name              = "OCP - VSO Operator Install"
   organization_name = "Default"
 }
+data "aap_job_template" "vault_credential" {
+  name              = "OCP - Vault Credential Setup"
+  organization_name = "Default"
+}
+
+resource "aap_job" "vault_credential" {
+  job_template_id = data.aap_job_template.vault_credential.id
+  extra_vars = jsonencode({
+    vault_addr            = var.vault_address
+    vault_namespace       = "admin"
+    controller_host       = var.aap_hostname
+    controller_username   = var.aap_username
+    controller_password   = var.aap_password
+    vault_role_id         = data.vault_approle_auth_backend_role_id.aap_controller.role_id
+    vault_secret_id       = vault_approle_auth_backend_role_secret_id.aap_controller.secret_id
+  })
+  wait_for_completion = false
+}
 
 resource "aap_job" "tf_admin_sa" {
   job_template_id = data.aap_job_template.tf_admin_sa.id

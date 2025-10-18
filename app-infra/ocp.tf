4c44fb0ae219203224e322a5f71a1f58da60c83f
benmiles-hashi <ben.miles@hashicorp.com>
2025-10-18 12:48:09 -0400

    added role binding to sa

--- app-infra/ocp.tf
+++ app-infra/ocp.tf
@@ -10,3 +10,23 @@ resource "kubernetes_service_account" "app_sa" {
     namespace = kubernetes_namespace.app.metadata[0].name
   }
 }
+
+resource "kubernetes_role_binding" "app_admin_binding" {
+  metadata {
+    name      = "${var.sa_name}-admin"
+    namespace = kubernetes_namespace.app.metadata[0].name
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "admin"
+  }
+
+  subject {
+    kind      = "ServiceAccount"
+    name      = kubernetes_service_account.app_sa.metadata[0].name
+    namespace = kubernetes_namespace.app.metadata[0].name
+  }
+}
+

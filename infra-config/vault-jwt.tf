314683d07a62f2ae1c81fb7a2497e4a0e581fbc8
benmiles-hashi <ben.miles@hashicorp.com>
2025-10-22 11:43:15 -0400

    fixes

--- infra-config/vault-jwt.tf
+++ infra-config/vault-jwt.tf
@@ -13,8 +13,8 @@ resource "vault_jwt_auth_backend" "jwt" {
   #namespace   = var.vault_namespace
   path        = "jwt-${var.cluster_id}"
   type        = "jwt"
-  jwks_url    = ephemeral.vault_kv_secret_v2.rosa_cluster_config.data["jwks_url"]
-  jwks_ca_pem = ephemeral.vault_kv_secret_v2.rosa_cluster_config.data["oidc_ca_chain"]
+  jwks_url    = ephemeral.vault_kv_secret_v2.rosa_cluster_config.data.jwks_url
+  jwks_ca_pem = ephemeral.vault_kv_secret_v2.rosa_cluster_config.data.oidc_ca_chain
 
   #default_role = "my-app"
   depends_on = [ aap_job.vso_install ]

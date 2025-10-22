fc9c11be36c1c108726aad060e41ee1f50cee3c8
benmiles-hashi <ben.miles@hashicorp.com>
2025-10-22 11:52:01 -0400

    resource read test

--- infra-config/vault-jwt.tf
+++ infra-config/vault-jwt.tf
@@ -1,10 +1,14 @@
 
 
 # --- Get ROSA cluster secrets from Vault ---
-ephemeral "vault_kv_secret_v2" "rosa_cluster_config" {
+resource "vault_kv_secret_v2" "rosa_cluster_config" {
   mount     = "openshift-rosa-${var.cluster_id}"
   name      = "config"
-  mount_id  = vault_mount.kv_mount.id
+
+  lifecycle {
+    read = true  # marks it as read-only, like a data source
+  }
+
   depends_on = [ aap_job.vso_install ]
 }
 
@@ -13,8 +17,8 @@ resource "vault_jwt_auth_backend" "jwt" {
   #namespace   = var.vault_namespace
   path        = "jwt-${var.cluster_id}"
   type        = "jwt"
-  jwks_url    = ephemeral.vault_kv_secret_v2.rosa_cluster_config.data.jwks_url
-  jwks_ca_pem = ephemeral.vault_kv_secret_v2.rosa_cluster_config.data.oidc_ca_chain
+  jwks_url    = vault_kv_secret_v2.rosa_cluster_config.data.jwks_url
+  jwks_ca_pem = vault_kv_secret_v2.rosa_cluster_config.data.oidc_ca_chain
 
   #default_role = "my-app"
   depends_on = [ aap_job.vso_install ]
